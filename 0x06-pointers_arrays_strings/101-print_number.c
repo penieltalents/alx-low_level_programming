5b83e32080c0c5bb265b071975ea0d0bb773b419
penieltalents <raphaelfabunmi@gmail.com>
2022-09-22 11:37:33 -0700

    101-print_number.c: Added a function- that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+/*
+ * File: 101-print_number.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: integer
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
